b77b1b703f3c06c6f4d9cc9edf0de4dc188b5c7c
João Vitor <jvmdo2008@gmail.com>
2017-10-06 16:11:29 -0300

    Adicionado range update na bit 2d

--- bit2d.cpp
+++ bit2d.cpp
@@ -1,20 +1,24 @@
-//
-// BIT 2D
-//
+struct BIT2D{
+	int v[maxn][maxn];
 
-ll BIT2D[maxn][maxn];
-
-void update(ll x, ll y, ll val)
-{
-     for(; x < maxn; x += x&-x)
-     	for(ll j = y; j < maxn; j += j&-j)
-        	BIT2D[x][j] += val;
-}
-ll query(ll x, ll y)
-{
-     ll sum = 0;
-     for(; x < maxn; x += x&-x)
-     	for(ll j = y; j < maxn; j += j&-j)
-        	sum += BIT2D[x][j];
-     return sum;
-}
+	void update(int x, int y, int val)
+	{
+		 for(; x < maxn; x += x&-x)
+		 	for(int j = y; j < maxn; j += j&-j)
+		    	v[x][j] += val;
+	}
+	int query(int x, int y)
+	{
+		 int sum = 0;
+		 for(; x > 0; x -= x&-x)
+		 	for(int j = y; j > 0; j -= j&-j)
+		    	sum += v[x][j];
+		 return sum;
+	}
+	int rangeupdate(int a1, int b1, int a2, int b2, int val){
+		update(a1,b1,val);
+		update(a2+1, b2+1, val);
+		update(a1,b2+1, -val);
+		update(a2+1,b1, -val);
+	}
+}bit;
